1c182bccfae63f09f02942c718cec24fa608281f
migdracios <migdracios@gmail.com>
2022-12-26 15:58:02 +0900

    add files b2b-soil

--- webplus/v7/projects/project03/app.py
+++ webplus/v7/projects/project03/app.py
@@ -1,10 +1,9 @@
 from flask import Flask, render_template, request, jsonify, redirect, url_for
 from pymongo import MongoClient
 
-
 app = Flask(__name__)
 
-client = MongoClient("mongodb+srv://sparta:coco0418@cluster0.ryaqzeb.mongodb.net/?retryWrites=true&w=majority")
+client = MongoClient("내 URL")
 db = client.dbsparta_plus_week3
 
 
@@ -16,7 +15,6 @@ def main():
 @app.route('/matjip', methods=["GET"])
 def get_matjip():
     matjip_list = list(db.matjips.find({}, {"_id": False}))
-    print(matjip_list)
     # 맛집 목록을 반환하는 API
     return jsonify({'result': 'success', 'matjip_list': matjip_list})
 
@@ -26,7 +24,6 @@ def like_matjip():
     title_receive = request.form["title_give"]
     address_receive = request.form["address_give"]
     action_receive = request.form["action_give"]
-    print(title_receive, address_receive, action_receive)
 
     if action_receive == "like":
         db.matjips.update_one({"title": title_receive, "address": address_receive}, {"$set": {"liked": True}})
